a45d7a67e0b14f70f34d7dfaf63e8037bff6a0fc
Aristotle <arismanios@hotmail.com>
2024-12-05 01:00:51 +0200

    Completed part 2 of 2 in day One

---
+++ day1part2.py
@@ -0,0 +1,34 @@
+
+with open('lists.txt', 'r') as file:
+    lines = file.readlines()
+
+left_column: list[int] = []
+right_column: list[int] = []
+
+
+for line in lines:
+
+    numbers = line.split()
+    if len(numbers) == 2:
+
+        left, right = map(int,numbers)
+        left_column.append(left)
+        right_column.append(right)
+
+left_column.sort()
+right_column.sort()
+
+sum = 0
+
+for num in left_column:
+    appearances = 0
+    for n in right_column:
+        if num == n:
+            appearances += 1
+
+    sum += (num * appearances)
+
+print(sum)
+
+
+
